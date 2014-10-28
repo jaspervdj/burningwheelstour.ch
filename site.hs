66a2572d8f4a51809e72b3deb18afbb37f3c7383
Jasper Van der Jeugt <m@jaspervdj.be>
2014-10-28 11:33:13 +0100

    Stuff

--- site.hs
+++ site.hs
@@ -1,9 +1,12 @@
+--------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-
 import Control.Applicative ((<$>))
 import System.Process (rawSystem)
+import System.FilePath (joinPath, splitPath, dropExtension)
 import Hakyll
 
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
     ----------------------------------------------------------------------------
@@ -14,12 +17,53 @@ main = hakyll $ do
             filePath       <- toFilePath <$> getUnderlying
             TmpFile target <- newTmpFile "out.png"
             _              <- unsafeCompiler $ rawSystem "inkscape"
-                ["-z" , "-e" , target , "-w", "1000" , filePath]
+                ["-z" , "-e" , target , "-w", "800" , filePath]
             makeItem $ TmpFile target
 
 
     ----------------------------------------------------------------------------
     -- .less files
-    match "style/*.less" $ do
+    allLessFiles <- makePatternDependency "style/*.less"
+    match "style/main.less" $ rulesExtraDependencies [allLessFiles] $ do
         route   $ setExtension "css"
-        compile $ getResourceBody >>= withItemBody (unixFilter "lessc" ["-"])
+        compile $ do
+            i         <- toFilePath <$> getUnderlying
+            TmpFile o <- newTmpFile "out.css"
+            _         <- unsafeCompiler $ rawSystem "lessc" [i, o]
+            makeItem $ TmpFile o
+    match "style/*.less" $ compile $ makeItem ()
+
+
+    ----------------------------------------------------------------------------
+    -- Pages
+    match "*.html" $ do
+        route $ indexRoute
+        compile $
+            getResourceBody                                              >>=
+            loadAndApplyTemplate "templates/default.html" defaultContext >>=
+            prettifyIndexRoutes                                          >>=
+            relativizeUrls
+
+    ----------------------------------------------------------------------------
+    -- Templates
+    match "templates/*.html" $ compile templateCompiler
+
+
+--------------------------------------------------------------------------------
+indexRoute :: Routes
+indexRoute = customRoute $ \identifier ->
+    let path  = splitPath (toFilePath identifier)
+        names = case dropExtension (last path) of
+            "index" -> ["index.html"]
+            n       -> [n, "index.html"]
+    in joinPath (init path ++ names)
+
+
+--------------------------------------------------------------------------------
+prettifyIndexRoutes :: Item String -> Compiler (Item String)
+prettifyIndexRoutes = return . fmap (withUrls prettify)
+  where
+    prettify []               = []
+    prettify url@(x : xs)
+        | "index.html" == url = []
+        | otherwise           = x : prettify xs
