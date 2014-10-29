1aa3f985b6533b228d1e2c43b35ca38de69c9f1e
Jasper Van der Jeugt <m@jaspervdj.be>
2014-10-29 11:01:32 +0100

    Events/archive

--- site.hs
+++ site.hs
@@ -1,9 +1,15 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 import           Control.Applicative ((<$>))
+import           Control.Applicative ((<$>), (<*>))
+import           Control.Monad       (filterM, mapM)
+import           Data.List           (nub, partition, sort)
+import qualified Data.Map            as M
 import           Data.Monoid         (mconcat)
+import           Data.Time           (UTCTime, toGregorian, utctDay)
 import           Hakyll
 import           System.FilePath     (dropExtension, joinPath, splitPath)
+import           System.Locale       (defaultTimeLocale)
 import           System.Process      (rawSystem)
 
 
@@ -68,6 +74,18 @@ main = hakyll $ do
 
 
     ----------------------------------------------------------------------------
+    -- Archive page
+    match "archive.html" $ do
+        route   $ indexRoute
+        compile $
+            getResourceBody                                          >>=
+            applyAsTemplate archiveCtx                               >>=
+            loadAndApplyTemplate "templates/default.html" archiveCtx >>=
+            prettifyIndexRoutes                                      >>=
+            relativizeUrls
+
+
+    ----------------------------------------------------------------------------
     -- Pages
     match "*.html" $ do
         route   $ indexRoute
@@ -106,10 +124,36 @@ prettifyIndexRoutes = return . fmap (withUrls prettify)
 --------------------------------------------------------------------------------
 eventsCtx :: Context String
 eventsCtx = mconcat
-    [ listField "events" eventCtx $ do
-        loadAll "events/*.html"
+    [ listField "events" eventCtx $ mapM load . fst =<< findEvents
     , pageCtx
     ]
+
+
+--------------------------------------------------------------------------------
+archiveCtx :: Context String
+archiveCtx = mconcat
+    [ listField "years" yearCtx $ do
+        (_, archived) <- findEvents
+        years         <-
+            mapM (fmap getYear . getItemUTC defaultTimeLocale) archived
+        mapM makeItem $ nub $ reverse $ sort years
+    , pageCtx
+    ]
+  where
+    getYear :: UTCTime -> Integer
+    getYear utct = let (year, _, _) = toGregorian (utctDay utct) in year
+
+    yearCtx :: Context Integer
+    yearCtx = mconcat
+        [ field "year" $ return . show . itemBody
+        , listFieldWith "events" eventCtx $ \yearItem -> do
+            (_, archived) <- findEvents
+            thisYear      <- filterM
+                (fmap ((== itemBody yearItem) . getYear) .
+                    getItemUTC defaultTimeLocale)
+                archived
+            mapM load thisYear
+        ]
 
 
 --------------------------------------------------------------------------------
@@ -125,3 +169,13 @@ pageCtx = mconcat
         return $ if fromFilePath p == underlying then "active" else "inactive"
     , defaultContext
     ]
+
+
+--------------------------------------------------------------------------------
+findEvents :: Compiler ([Identifier], [Identifier])  -- current, archived
+findEvents = do
+    metadatas <- getAllMetadata "events/*.html"
+    let (current, archived) =
+            partition (\(_, md) -> M.lookup "archived" md /= Just "true") $
+            metadatas
+    return (map fst current, map fst archived)
