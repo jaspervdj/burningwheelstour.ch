0ce7b148b4aea5d88e3f5ecb6543b2f158bc0c83
Jasper Van der Jeugt <m@jaspervdj.be>
2014-11-06 11:31:37 +0100

    Add event flyers and descriptions

--- site.hs
+++ site.hs
@@ -1,7 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Control.Applicative ((<$>))
-import           Control.Applicative ((<$>), (<*>))
+import           Control.Applicative ((<$>), (<*>), empty)
 import           Control.Monad       (filterM, mapM)
 import           Data.List           (nub, partition, sort)
 import qualified Data.Map            as M
@@ -9,7 +8,7 @@ import           Data.Monoid         (mconcat)
 import           Data.Time           (UTCTime, toGregorian, utctDay)
 import           Hakyll
 import           System.FilePath     (dropExtension, joinPath, splitDirectories,
-                                      splitPath)
+                                      splitPath, takeBaseName)
 import           System.Locale       (defaultTimeLocale)
 import           System.Process      (rawSystem)
 
@@ -31,6 +30,26 @@ main = hakyll $ do
     match "js/*.js" $ do
         route   idRoute
         compile copyFileCompiler
+
+
+    ----------------------------------------------------------------------------
+    -- Flyers (big)
+    match "images/flyers/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+
+    ----------------------------------------------------------------------------
+    -- Flyers (small)
+    match "images/flyers/*" $ version "small" $ do
+        route   $ customRoute $ \identifier ->
+            moveDownIntoDirectory "small" (toFilePath identifier)
+        compile $ do
+            i         <- toFilePath <$> getUnderlying
+            TmpFile o <- newTmpFile "out.jpg"
+            _         <- unsafeCompiler $
+                            rawSystem "convert" ["-resize", "300x", i, o]
+            makeItem $ TmpFile o
 
 
     ----------------------------------------------------------------------------
@@ -121,6 +140,16 @@ eventCtx :: Context String
 eventCtx = mconcat
     [ field "year" $
         fmap (show . getYear) . getItemUTC defaultTimeLocale . itemIdentifier
+    , functionField "flyer" $ \args item -> do
+        let bn = takeBaseName $ toFilePath $ itemIdentifier item
+            pt = fromGlob ("images/flyers/" ++ bn ++ ".*")
+            vp = case args of
+                [v] -> hasVersion v
+                _   -> hasNoVersion
+        identifiers <- getMatches (pt .&&. vp)
+        case identifiers of
+            [identifier] -> maybe empty toUrl <$> getRoute identifier
+            _            -> empty
     , activeClassField
     , defaultContext
     ]
@@ -176,3 +205,13 @@ findEvents = do
 --------------------------------------------------------------------------------
 getYear :: UTCTime -> Integer
 getYear utct = let (year, _, _) = toGregorian (utctDay utct) in year
+
+
+--------------------------------------------------------------------------------
+moveDownIntoDirectory
+    :: String -> FilePath -> FilePath
+moveDownIntoDirectory dir = joinPath . go . splitDirectories
+  where
+    go []       = []
+    go [x]      = dir : [x]
+    go (x : xs) = x : go xs
